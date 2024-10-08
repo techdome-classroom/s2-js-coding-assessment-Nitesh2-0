b60e744396b3f33b679153361c5ed510b30c7551
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-10-08 14:37:43 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,10 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    let res = 0;
-    const rome
+var romanToInt = function (s) {
+  let res = 0;
+  const roman = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }; 
+  for(let )
 };
 
 
-module.exports={romanToInt}
+module.exports = { romanToInt }

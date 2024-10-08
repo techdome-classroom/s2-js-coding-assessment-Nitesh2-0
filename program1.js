6b51b1608709bc0a05c2af5fd118f6c05f423ed8
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-10-08 14:27:25 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,24 +4,7 @@
  */
 var isValid = function (s) {
   let stack = [];
-  let array = s.split("");
-  const mapVal = new Map();
-  mapVal.set('{', '}');
-  mapVal.set('(', ')');
-  mapVal.set('[', ']');
-
-  let temp = 0;
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i; j < array.length; j++) {
-      if (mapVal.get(array[i] === array[j])) {
-        array[i], array[j] = 0;
-        temp += 2
-      }
-    }
-  }
-  if (array.length == temp) {
-    return true;
-  }
+  
   return false;
 };
 

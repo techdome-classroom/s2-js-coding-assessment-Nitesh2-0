2b059a5158bf441ec7136c4f04091423202ea3a4
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-10-08 14:21:51 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,15 +12,16 @@ var isValid = function (s) {
   const stack = [];
   for (let i = 0; i < s.length; j++) {
     for (let j = 0; j < barackets.length; j++) {
-      if(s[i] === barackets[j][0]){
+      if (s[i] === barackets[j][0]) {
         stack.push(s[i]);
-      }else if(s[i] === barackets[j][1]){
-        if(stack.length === 0 || stack.pop() !== barackets[j][0]){
+      } else if (s[i] === barackets[j][1]) {
+        if (stack.length === 0 || stack.pop() !== barackets[j][0]) {
           return false
         }
       }
     }
   }
+  return stack.length === 0;
 };
 
 module.exports = { isValid };

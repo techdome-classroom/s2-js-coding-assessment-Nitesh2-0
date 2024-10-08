c8677c7c1fd229f247aacb98b38112f68aab2cf0
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-10-08 14:20:30 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -17,25 +17,6 @@ var isValid = function (s) {
       }
     }
   }
-  let array = s.split("");
-  const mapVal = new Map();
-  mapVal.set('{', '}');
-  mapVal.set('(', ')');
-  mapVal.set('[', ']');
-
-  let temp = 0;
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i; j < array.length; j++) {
-      if (mapVal.get(array[i] === array[j])) {
-        array[i], array[j] = 0;
-        temp += 2
-      }
-    }
-  }
-  if (array.length == temp) {
-    return true;
-  }
-  return false;
 };
 
 module.exports = { isValid };

ce65f81c5abe7ca828da3c33e02b35220fbdb7d0
Nitesh2-0 <niteshkumar61725@gmail.com>
2024-10-08 14:05:47 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,26 +2,26 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let array = s.split("");
-    const mapVal = new map(); 
-    map.set('{','}');
-    map.set('(',')'); 
-    map.set('[',']'); 
-    
-    let temp = 0; 
-    for(let i=0; i<s.length; i++){
-      for(let j=i; j<array.length; j++){
-        if(map.get(array[i] === array[j])){
-          array[i],array[j]=0;
-          t += 2
-        }
+var isValid = function (s) {
+  let array = s.split("");
+  const mapVal = new map();
+  map.set('{', '}');
+  map.set('(', ')');
+  map.set('[', ']');
+
+  let temp = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      if (map.get(array[i] === array[j])) {
+        array[i], array[j] = 0;
+        t += 2
       }
     }
-    if(array.length == temp){
-      return true;
-    }
-    return false;
+  }
+  if (array.length == temp) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = { isValid };
